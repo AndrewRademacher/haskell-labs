ed40033cf8cf7b078bb3dd618e558aea7a1e1335
Andrew Rademacher <andrewrademacher@gmail.com>
2013-11-25 10:29:54 -0600

    Added JavaScript implementation of Fibs.

---
+++ fibs/Fibs.js
@@ -0,0 +1,15 @@
+
+function getFibs (n) {
+    var fibs = [1, 1];
+
+    for (var i = 2; i <= n; i++) 
+        fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]);
+
+    return fibs;
+}
+
+//  Main
+var n   = Number(process.argv[2]),
+    lst = getFibs(n);
+
+console.log(lst[lst.length - 1]);
